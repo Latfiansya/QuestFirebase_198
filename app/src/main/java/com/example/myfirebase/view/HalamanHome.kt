89be9a9b66d9648f4dc3b2ee17ca2659a0c84810
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2026-01-05 23:57:06 +0700

    fix: Update R class import path in HalamanHome
    Correct the import statement for the `R` class in `HalamanHome.kt`. The path has been changed from `androidx.compose.material3.R` to the project-specific `com.example.myfirebase.R`.

--- app/src/main/java/com/example/myfirebase/view/HalamanHome.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanHome.kt
@@ -20,7 +20,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.R
+import com.example.myfirebase.R
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
